c1ae6ff2a9e0fec10dec182027af48bfb41602bb
Dirk Arnez <smalldirkalex@gmail.com>
2024-08-23 19:56:36 +0800

    - update main.go

--- main.go
+++ main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-    "context"
+	"context"
 	"fmt"
-	"os"
-    "os/exec"
+	"os/exec"
 	"time"
 )
 
 func main() {
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	fmt.Println("Start")
+	cmd := exec.CommandContext(ctx, "sleep", "5") //os.Args[0], os.Args[1:]...)
 
-    cmd := exec.CommandContext(ctx, os.Args[0], os.Args[1:]...)
+	_, err := cmd.CombinedOutput()
 
-    _, err := cmd.CombinedOutput()
-
-    if (ctx.Err() == context.DeadlineExceeded) {
+	if ctx.Err() == context.DeadlineExceeded {
 		fmt.Println("Command was killed")
-    }
+	}
 
-    if err != nil {
-        fmt.Println("If the command was killed, err will be \"signal: killed\"")
+	if err != nil {
+		fmt.Println("If the command was killed, err will be \"signal: killed\"")
 		fmt.Println("If the command wasn't killed, it contains the actual error, e.g. invalid command")
-    }
+	} else {
+		fmt.Println("Done")
+	}
 }
